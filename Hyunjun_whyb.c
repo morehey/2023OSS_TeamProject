1de942a665e5fc8ca89458e06382ae97409347e4
Your Name <junono1@naver.com>
2023-05-20 05:15:43 +0900

    random pick

---
+++ Hyunjun_whyb.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "whyb.h"
+#include <time.h>
+
+int loadData(food *f[]){
+   const char* filename = "menu_data.txt";
+    FILE* file = fopen(filename, "r");
+
+    if (file != NULL) {
+
+        int dataCount = 0;
+        char line[256];
+        while (fgets(line, sizeof(line), file) != NULL) {
+            scanf(line, "9s %d %d %d %d %d", 
+                   f[dataCount]->name,
+                   &f[dataCount]->type,
+                   &f[dataCount]->price,
+                   &f[dataCount]->cnt,
+                   &f[dataCount]->del,
+                   &f[dataCount]->get
+                   );
+
+            dataCount++;
+        }
+
+        fclose(file);
+
+        printf("data loaded\n");
+
+        // return data count
+        return dataCount;
+    } else {
+        printf("there is no data\n");
+
+        // return 0 because there is nodata
+        return 0;
+    }
+}
+
+
+int selectMenuTwo(){
+     //menu 1 : random pick
+     //menu 2 : worldcuup pick
+     //menu 0 : quit
+    int num;
+
+    printf(" 1 : totaly random pick \n");
+    printf(" 2 : pick by world cup \n" );
+    printf(" 0 : quit ");
+
+    scanf("%d",&num);
+
+    return num;
+
+}
+
+void randomPick(food *f[], int count){
+     //SelectFoodType 통해서 종류 정하기
+     //type 일치, delete 안된 메뉴 중 random으로 나온 값 print
+
+    int type;
+    int judge = 0;
+    srand(time(NULL));
+
+    while(judge==0){
+    int randomNumber = rand() % 100;
+
+    type = selectFoodType;
+
+    if(f[randomNumber]->s)
+    }
+    
+
+}
+
+void worldCupPick(food *f[], int count){//현준
+     //SelectFoodType 통해서 종류 정하기
+     //type 일치, delete 안된 메뉴 중 월드컵 실시
+     //최종 메뉴 print
+}
